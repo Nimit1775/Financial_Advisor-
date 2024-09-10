a0464e3e356099fd7f5ed004016671a39924802f
nimit sodhani <nimitsodhani1@gmail.com>
2024-09-10 17:54:11 +0530

    have to make in 2 days fas fas

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import Header from "./_components/Header.jsx";
+import Hero from "./_components/Hero.jsx";
 
 export default function Home() {
-  return <Header />
+  return  (
+    <div>  
+  <Header />
+   <Hero/>
+   </div>
+  ) ; 
 }
